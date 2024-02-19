09de1b772a6b01a75eb0141762a9500b2f9ce725
Omaribz <cynthiamyud@gmail.com>
2024-02-19 19:18:16 +0300

    Change Images height

--- quiz-app/src/components/QuizCard.tsx
+++ quiz-app/src/components/QuizCard.tsx
@@ -20,9 +20,9 @@ const QuizCard = ({ searchValue }: QuizCardProps) => {
     <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={5} padding="10px">
       {filteredQuizes.map((quiz) => (
         <Card key={quiz.id} borderRadius={10} overflow="hidden">
-          <Image src={quiz.img} />
+          <Image height="100%" src={quiz.img} />
           <CardBody>
-            <Heading fontSize="2xl">{quiz.name}</Heading>
+            <Heading fontSize="xl">{quiz.name}</Heading>
             <Text color="#1A365D" mt={2}>
               {quiz.questions.length} questions
             </Text>
